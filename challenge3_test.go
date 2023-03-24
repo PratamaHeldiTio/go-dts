90fe499f59b8b5a24d9902eb4748d47fc98e77ad
pratama <pratamahelditio@gmail.com>
2023-03-24 13:43:32 +0700

    challange 3

--- challenge3_test.go
+++ challenge3_test.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestChallangeThree(t *testing.T) {
+	word := "selamat malam"
+
+	data := make(map[string]int)
+
+	for _, value := range word {
+		fmt.Println(string(value))
+		data[string(value)]++
+	}
+	fmt.Println(data)
+
+}
